f95eed0f0f5ebb13fc9f13d64c28fe40b6092e99
Byungwoo Jeon <16890077+byung-u@users.noreply.github.com>
2021-05-30 13:53:02 +0900

    Update weeklymeetup_noti_2days_ago.py
    Notify only monday, fix message

--- weeklymeetup_noti_2days_ago.py
+++ weeklymeetup_noti_2days_ago.py
@@ -22,17 +22,12 @@ def send_slack_message(slack_data):
 
 
 def noti_1day_ago():
-    # if datetime.now().weekday() != 0:  # only Monday FIXME: Test 이후에 주석 제거
-    #     print('Notify only Monday')
-    #     return
+    if datetime.now().weekday() != 0:
+        print('Notify only Monday')
+        return
     title = (f'{"내일 회의 있는 날이에요!"}')
     message = (f'{":arrow_right: 참여가 가능해요 :o:"}\n'
                f'{":arrow_right: 참여가 힘들어요 :x:"}\n'
-               f'{" "}\n'
-               f'{"*타임테이블*"}\n'
-               f'{"```"}\n'
-               f'{"- 19:30 ~ 20:00 일주일간 잘 지내셨나요?"}\n'
-               f'{"- 20:00 ~ ??:?? 정기회의```"}\n'
                f'{" "}\n'
                f'{"  더불어 오늘 이야기하고 싶으신 내용이 있다면"}\n'
                f'{"  회의록에 먼저 작성해주세요~ :wink:"}\n'
